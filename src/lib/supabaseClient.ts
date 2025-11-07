aedd59d429998ed2363cd2069f06881333979b9c
Denis Cugler <deniscugler@gmail.com>
2025-11-07 19:58:12 -0300

    feat: integração Supabase - arquivo de configuração criado

---
+++ src/lib/supabaseClient.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://ukumsztvbcurhuopqltv.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVrdW1zenR2YmN1cmh1b3BxbHR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1NDY0ODEsImV4cCI6MjA3ODEyMjQ4MX0._hTxqszcLdTSFJO09bQqyImHQOouen92UXLn3RwnLpg';
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
