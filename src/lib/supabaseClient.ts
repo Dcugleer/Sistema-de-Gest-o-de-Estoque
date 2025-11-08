1356e5978d93108b4ae2baa06a575d19e570f902
Denis Cugler <deniscugler@gmail.com>
2025-11-08 11:06:38 -0300

    fix: supabase agora usa variáveis de ambiente e dependência instalada

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://ukumsztvbcurhuopqltv.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVrdW1zenR2YmN1cmh1b3BxbHR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI1NDY0ODEsImV4cCI6MjA3ODEyMjQ4MX0._hTxqszcLdTSFJO09bQqyImHQOouen92UXLn3RwnLpg';
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
